5b6b9d9d27283ec63a757ccdb6e62322ba74ba23
Elijah Styler <elijahstyler@gmail.com>
2026-02-03 20:17:49 -0700

    howdy fellr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to the dark abyss of depression</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentations</p>
