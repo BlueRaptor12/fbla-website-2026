d35878e6756c945f2c09e991bd9b345ed008ca76
bf-bonkus <bf.bonkus@proton.me>
2026-02-06 08:18:22 -0700

    move navbar into svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<header>
-    <h1>Skyridge High School Lost and Found</h1>
-</header>
-
-
-<nav style="text-align: center;">
-    <a href="/">Home</a> &nbsp;
-    <a href="/report">Report Lost Item</a> &nbsp;
-    <a href="/view">View Found Items</a> &nbsp;
-    <a href="/contact">Contact Us</a>
-</nav>
-
 <h1>HOME</h1>
